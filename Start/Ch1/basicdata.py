9e5b44afb41be0ed52f4993ceded46b649fd6dd1
Fernanda Fernandes <73358459+2timesfer@users.noreply.github.com>
2024-02-08 13:23:40 +0000

    Chap.1 solved

--- Start/Ch1/basicdata.py
+++ Start/Ch1/basicdata.py
@@ -9,10 +9,13 @@ with open("../../sample-weather-history.json", "r") as weatherfile:
     weatherdata = json.load(weatherfile)
 
 # TODO: What was the warmest day in the data set?
-
+max_temp = max(weatherdata, key=lambda x: x['tmax'])
+print(f"Dia mais quente: {max_temp['date']}")
 
 # TODO: What was the coldest day in the data set?
-
+min_temp = min(weatherdata, key=lambda x: x['tmin'])
+print(f"Dia mais frio: {min_temp['date']}")
 
 # TODO: How many days had snowfall?
-
+nevou = [day for day in weatherdata if day['snow']>0]
+print(f"Nevou {len(nevou)} dias")
